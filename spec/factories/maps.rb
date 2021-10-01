0fd5f0874d44d208e4df5301c1b331cf0533cdec
Tom-SAS <tommakuhari@gmail.com>
2021-10-01 23:56:59 +0900

    公園投稿機能の実装

--- spec/factories/maps.rb
+++ spec/factories/maps.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :map do
-    text {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
+    name {Faker::Name.name}
+    address {"神奈川県横浜市青葉区美しが丘1−1"}
+    message {Faker::Lorem.sentence}
     association :user 
+    after(:build) do |map|
+      map.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Map, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @map = FactoryBot.build(:map)
+  end
+  describe '公園情報投稿' do
+    context '登録できる時' do
+      it '全てのカラムを正常入力したら登録できる' do
+        expect(@map).to be_valid
+      end
+    end
+  end
+    context '登録できない時' do
+      it '公園名が空だったら登録できない' do
+        @map.name = ''
+        @map.valid?
+        expect(@map.errors.full_messages).to include "Name can't be blank"
+      end
+      it '住所が空だったら登録できない' do
+        @map.address = ''
+        @map.valid?
+        expect(@map.errors.full_messages).to include "Address can't be blank"
+      end
+      it 'メッセージが空だったら登録できない' do
+        @map.message = ''
+        @map.valid?
+        expect(@map.errors.full_messages).to include "Message can't be blank"
+      end
+    end
 end
